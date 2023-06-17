656a3b73fd48508d0bd25e232ec05b94e7214ab5
Dicadillo <91073255+Dicadillo@users.noreply.github.com>
2023-06-17 22:46:29 +0200

    ultimos retoques rutas

--- Allfor1TFG/src/Controller/ProfileController.php
+++ Allfor1TFG/src/Controller/ProfileController.php
@@ -3,16 +3,38 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Obtener el usuario actualmente autenticado
+        $user = $this->getUser();
+
+        // Procesar el formulario de actualización de datos del usuario
+        if ($request->isMethod('POST')) {
+            $newData = $request->request->all();
+
+            // Actualizar los datos del usuario con los nuevos valores
+            $user->setName($newData['name']);
+            $user->setEmail($newData['email']);
+
+            // Guardar los cambios en la base de datos
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigir a la página de perfil con un mensaje de éxito
+            $this->addFlash('success', 'Los datos del usuario se han actualizado correctamente.');
+            return $this->redirectToRoute('app_profile');
+        }
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user,
         ]);
     }
 }
